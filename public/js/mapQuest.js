e126a1156a7823f980340ddff1e5dadbc5be5159
Peony Lum <peonylum@MacBook-Pro-2.local>
2021-02-25 10:29:56 -0800

    Complete lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = '8foPnuXf3LODyIZeeYGb1MQA7kQUq5jS'
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [32.8785, -117.2359],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 14,
+	  zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.88,-117.236]).addTo(map)
 }
